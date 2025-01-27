29e9ba2c01b4c7bc14ba8ec4d393ae5827c745d6
Keaton Sneed <kjsneed2@gmail.com>
2025-01-26 23:37:51 -0600

    Added color definitions to css and added changing color feature

--- stky/app/components/notes.jsx
+++ stky/app/components/notes.jsx
@@ -11,11 +11,22 @@ export function Notes()
 
     function Note(key)
     {
+        function changeColor(colorNumber)
+        {
+            document.getElementById(key).style.borderColor = window.getComputedStyle(document.documentElement).getPropertyValue(`--noteBorder${colorNumber}`)
+            document.getElementById(key).querySelector('.grabber').style.color = window.getComputedStyle(document.documentElement).getPropertyValue(`--noteBorder${colorNumber}`)
+        }
         return (
-        <div className="note" key={key}>
+        <div className="note" key={key} id={key}>
             <p className = "grabber">: : : : : : : : : : : :</p>
             <input className = "notetitle" placeholder="Note Title"/>
             <textarea className = "notetext" placeholder="Enter your text here..."></textarea>
+            <div className = "noteColors">
+                <button className='cb cb1' onClick={()=>changeColor(1)}> </button>
+                <button className='cb cb2' onClick={()=>changeColor(2)}> </button>
+                <button className='cb cb3' onClick={()=>changeColor(3)}> </button>
+                <button className='cb cb4' onClick={()=>changeColor(4)}> </button>
+            </div>
         </div>
         )
     }
@@ -51,9 +62,8 @@ export function Notes()
             }
 
         return(
-            <div id="emptyfield" onClick={handleClick} style={add}>
-                <img className="icon plusicon" src="/icons/plus.svg"/>
-                <h2>Click "+" to add a new note.</h2>
+            <div id="plusfield" onClick={handleClick} style={add}>
+                <img id="plusnormal" className="icon plusicon" src="/icons/plus.svg"/>
             </div>
     )
     }
